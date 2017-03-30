29231a9e2428bf5f86bc1bee59ed671b3e84ba80
ThermalVB <wyatt.horejsi@gmail.com>
2017-03-30 16:10:32 -0700

    added admin controller and FileManager

---
+++ DojoCMS/Controllers/AdminController.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DojoCMS.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DojoCMS.Controllers
+{
+    public class AdminController : Controller
+    {
+        [HttpGet]
+        [Route("")]
+        public IActionResult Admin()
+        {   
+            ViewBag.Errors = new List<string>();
+            ViewBag.LoginErrors = new List<string>();
+            return View("AdminIndex");
+        }
+    }
+}
