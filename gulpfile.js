8a535fd03c38515ae22c1ad2b3a718adfc5de26d
ThermalVB <wyatt.horejsi@gmail.com>
2017-03-30 12:57:51 -0700

    Revert "still fixing shit"
    This reverts commit 9fda4a394641e03700883fbcf667bc386cf43c53.

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp');
-var DestinationDirectory = 'someDirectory'
-var OutputFileName = 'someFileName'
-
-gulp.task('WriteFile', function() {
-    gulp.src('{./inputFile}'), {base: DestinationDirectory}
-  .pipe(gulp.dest(OutputFileName))
-});
